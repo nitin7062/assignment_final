dee42cb6dfb98c695fd76d3f3adc0b4b0a997572
Nitin Koolwal <nitin.koolwal@fraazo.com>
2022-11-17 13:19:30 +0530

    refractored

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   def index
     @users = current_user.role
     if @users =="Mortgagee"
-      @users= User.where.not(role:"Mortgagee")
+      @users= User.where(role:"Mortgagor")
     else
-      @users =User.where(role:"Mortgagor")
+      @users =User.where(role:"Mortgagee")
     end
   end
 
